cf81569fb5a1d8a544cf2706ea9d3473407639f8
Aravind Potluri <aravindswami135@gmail.com>
2024-06-12 20:03:27 +0000

    code v1.2.0: 1. Created a structure with functions. 2. Made proper error handling in each step.

--- client.py
+++ client.py
@@ -1,18 +1,73 @@
+################################################################
+# Author        : Mounika Musugu <mounikamounika30m@gmail.com>
+# Description   : This TCP based client, connects to TCP server
+#                 to send and recevie Msgs in a loop.
+################################################################
+
+# Macros
+serverIP = "127.0.0.1"  # Defaulting local IP
+serverPORT = 8080       # Server Port 
+# Imports
 import socket
 
-try:
-    server_hostname = socket.gethostname()
-    server_ip = socket.gethostbyname(server_hostname)
-    s = socket.socket()
-    s.connect((server_ip,8080))
+# Creating socket
+def create_socket():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+        print("[+] Socket successfully created")
+        return sock
+    except:
+        print(f"[-] Socket creation failed")
+        sock.close()
+        exit()
+
+# Connect to the server
+def connect_to_server(sock, serverIP, serverPORT):
+    try:
+        sock.connect((serverIP, serverPORT))
+        print("[+] Successfully connected to server")
+    except:
+        print("[-] Connection failed")
+        sock.close()
+        exit()
+
+# Data communication
+def data_exchange(sock):
     while True:
-        msg=input("enter client msg: ")
-        s.send(bytes(msg,'utf-8'))
-        recvmsg=s.recv(1024).decode()
-        print(recvmsg)
-except socket.error as err:
-    print(f"Socket error: {err}")
-finally:
-    s.close()
+        try:
+            # Sending the user input to server
+            sock.send(input("\n[#] Enter the msg: ").encode('UTF-8'))
 
+            # Receive the response from server
+            data = sock.recv(1024) # Assuming MAX data to be received is 1024 Bytes.
+            if len(data) == 0:
+                raise socket.error # Broken pipe error
+            print(f"[#] Response from server: {data.decode('UTF-8')}")
 
+        except KeyboardInterrupt:
+            print(" [!] Client shutting down.")
+            break
+
+        except socket.error:
+            print("[!] Connection closed by server")
+            break
+
+        except Exception as err:
+            print(f"[!] {str(err)}")
+            break
+
+if __name__ == "__main__":
+    # Creating the socket
+    sock = create_socket()
+
+    # Get server IP from user
+    serverIP = input("\n[#] Enter server IP: ")
+
+    # Connecting to server
+    connect_to_server(sock, serverIP, serverPORT)
+
+    # Start communication
+    data_exchange(sock)
+
+    # Closing the connection
+    sock.close()
--- server.py
+++ server.py
@@ -1,20 +1,89 @@
+################################################################
+# Author        : Mounika Musugu <mounikamounika30m@gmail.com>
+# Description   : This TCP based server, waits for TCP client
+#                 Msgs and then sends our Msg in loop.
+################################################################
+
+# Macros
+serverIP = "0.0.0.0"    # Accept from any IP.
+serverPORT = 8080       # Server Port  
+
+# Imports
 import socket
 
-try:
-    s = socket.socket()
-    print("socket created")
-    server_ip = ''
-    s.bind((server_ip,8080))
+# Create socket
+def create_socket():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+        print("[+] Socket successfully created")
+        return sock
+    except:
+        print(f"[-] Socket creation failed")
+        sock.close()
+        exit()
+
+# Bind socket
+def bind_socket(sock, serverIP, serverPORT):
+    try:
+        sock.bind((serverIP, serverPORT))
+        print(f"[+] socket successfully binded on port {serverPORT}")
+    except:
+        print(f"[-] Socket binding failed")	
+        sock.close()
+        exit()
+
+# Establishing the connection
+def listen_for_connections(sock):
+    try:
+        sock.listen(2)
+        print("[+] wating for connection...")
+        clientSock, clientAddr = sock.accept()
+        print(f"[+] Accepted connection from : {clientAddr}\n")
+        return clientSock
+    except:
+        print("[-] Connection establishment faild")
+        sock.close()
+        exit()
+
+# Data communication
+def data_exchange(sock, clientSock):
     while True:
-        s.listen(3)
-        print("waiting for connection")
-        client,addr=s.accept()
-        print(addr)
-        recvmsg= client.recv(1024).decode()
-        print("received message: ",recvmsg)
-        msg=input("enter message from server: ")
-        client.send(bytes(msg,'utf-8'))
-except socket.error as err:
-    print(f"Socket error: {err}")
-finally:
-    client.close()
+        try:
+            # Receive data from any client
+            data = clientSock.recv(1024) # Assuming MAX data to be received is 1024 Bytes.
+            if len(data) == 0:
+                raise socket.error # To escape the infinte loop, after breaking the tcp pipeline
+            msg = data.decode('UTF-8')
+            print(f"[#] Received message: {msg}")
+
+            # Reply msg to client
+            msg = input("\n[#] Enter the msg: ")
+            clientSock.send(msg.encode('UTF-8'))
+
+        except KeyboardInterrupt:
+            print(" [!] Server shutting down.")
+            break
+
+        except socket.error:
+            print("[!] Connection closed by client")
+            break
+
+        except Exception as err:
+            print(f"[!] {str(err)}")
+            break
+
+if __name__ == "__main__":
+    # Creating socket
+    sock = create_socket()
+
+    # Bind socket
+    bind_socket(sock, serverIP, serverPORT)
+
+    # Wait for connections
+    clientSock = listen_for_connections(sock)
+
+    # Data exchange
+    data_exchange(sock, clientSock)
+
+    # Closing the connection
+    sock.close()
